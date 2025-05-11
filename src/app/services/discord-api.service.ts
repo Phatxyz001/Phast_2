92a6f5aad39371898d09d4e4c6f877733f3b4ea2
Phast <107768700+Phatxyz001@users.noreply.github.com>
2025-05-11 16:38:21 +0700

    Update discord-api.service.ts

--- src/app/services/discord-api.service.ts
+++ src/app/services/discord-api.service.ts
@@ -1,18 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Profile } from '../models/discord-profile.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiscordApiService {
+  private apiUrl = environment.production
+  ? '/api/proxy?id=' // Vercel serverless function
+  : '/discord/data/profile/'; // Local proxy
 
-  urlDiscordApi: string = 'https://khaidevapi.onrender.com/discord/data/profile/'
+  constructor() { }
 
-constructor(private http: HttpClient) { }
+  async getDiscordUser(id: string): Promise<Profile> {
+    try {
+      const response = await fetch(this.apiUrl + id, {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Requested-With': 'XMLHttpRequest'
+        },
+      });
 
-  getDiscordUser(id: string): Observable<Profile> {
-    return this.http.get<Profile>(this.urlDiscordApi + id);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data: Profile = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw error;
+    }
   }
 }
